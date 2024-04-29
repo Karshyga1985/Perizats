014f3162213fcaf84e20aeedcbb98770884eab28
mehmetCt <mehmet@cybertekschool.com>
2024-04-29 15:44:17 -0400

    Project init with US01

--- src/test/java/com/library/steps/APIStepDefs.java
+++ src/test/java/com/library/steps/APIStepDefs.java
@@ -1,6 +1,61 @@
 package com.library.steps;
 
 
+import com.library.utility.ConfigurationReader;
+import com.library.utility.LibraryAPI_Util;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
+import io.restassured.response.ValidatableResponse;
+import io.restassured.specification.RequestSpecification;
+import org.hamcrest.Matchers;
+import org.junit.Assert;
+
+import static org.hamcrest.Matchers.*;
+
 public class APIStepDefs {
+
+    RequestSpecification givenPart;
+    Response response;
+    ValidatableResponse thenPart;
+
+    /**
+     *  US - 01
+     *
+     */
+
+    @Given("I logged Library api as a {string}")
+    public void i_logged_library_api_as_a(String userType) {
+        givenPart= RestAssured.given().log().uri()
+                .header("x-library-token", LibraryAPI_Util.getToken(userType));
+    }
+    @Given("Accept header is {string}")
+    public void accept_header_is(String accept) {
+        givenPart.accept(accept);
+
+    }
+    @When("I send GET request to {string} endpoint")
+    public void i_send_get_request_to_endpoint(String endpoint) {
+         response = givenPart.when().get(ConfigurationReader.getProperty("library.baseUri") + endpoint)
+                .prettyPeek();
+
+       thenPart = response.then();
+
+    }
+    @Then("status code should be {int}")
+    public void status_code_should_be(Integer statusCode) {
+
+        thenPart.statusCode(statusCode);
+    }
+    @Then("Response Content type is {string}")
+    public void response_content_type_is(String contentType) {
+        thenPart.contentType(contentType);
+    }
+    @Then("Each {string} field should not be null")
+    public void each_field_should_not_be_null(String path) {
+        thenPart.body(path, everyItem(notNullValue()));
+    }
 
 }
